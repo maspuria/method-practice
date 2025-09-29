66167a808acec07bd03a6ded46ddc4153aa3738e
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-29 13:59:30 -0500

    format update commit

---
+++ src/main/java/com/pluralsight/Format.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Format {
+    public static void main(String[] args) {
+        formatName("Mari","Aspuria");
+    }
+    public static void formatName(String first, String last){
+        System.out.println(last + ", " + first);
+    }
+}
