287abb7aac5df1939d373b03dc559b726a1b58c7
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-29 13:59:58 -0500

    hello update commit

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
 public class Hello {
+    //     Create main()
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodmorning();
+    }
+
+    //     Create a method sayHello() that prints "Hello, World!"
+    public static void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    //    Create a method sayGoodbye() that prints "Goodbye!"
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+    // Create a method sayGoodmorning() that prints "Good morning!"
+    public static void sayGoodmorning(){
+        System.out.println("Good morning!");
+    }
+
+
 }
